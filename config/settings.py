3159d659dfe7599259e0f949b2d60683887c6072
Luis Humberto Orozco Cabrera <humbledore@gmail.com>
2021-04-15 20:58:38 -0400

    Added STATIC_Root for collecstatic

--- config/settings.py
+++ config/settings.py
@@ -123,3 +123,6 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# See: https://docs.djangoproject.com/en/dev/ref/settings/#static-root
+STATIC_ROOT = str(os.path.join(BASE_DIR, 'staticfiles/'))
