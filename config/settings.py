687e9b82661ee41561e93c2004c8230ecdbb0351
Luis Humberto Orozco Cabrera <humbledore@gmail.com>
2021-04-15 21:27:54 -0400

    Added new settings

--- config/settings.py
+++ config/settings.py
@@ -10,11 +10,13 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 import os
+import environ
 from pathlib import Path
+
+env = environ.Env()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
@@ -22,8 +24,15 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
+ENV_TYPE = os.getenv('ENV_TYPE')
+
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+if ENV_TYPE == 'local':
+    os.environ['DEBUG'] = 'True'
+    os.environ['DATABASE_URL'] = 'postgres://postgres:postgres@db:5432/postgres'
+    os.environ['REDIS_HOST'] = 'redis'
+
+DEBUG = os.getenv('DEBUG')
 
 ALLOWED_HOSTS = []
 
@@ -74,10 +83,7 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': env.db('DATABASE_URL')
 }
 
 
